c429f980bd62ee42161c7a0cd2f18f297cb0d0b6
Raj <Raj@Ldesk.local>
2017-06-27 18:39:10 -0700

    build.xml changes

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,6 +1,6 @@
 public class Rectangulator {
 	public static void main(String[] args) {
-		int lenght = Integer.parseInt(args[0]);
+		int length = Integer.parseInt(args[0]);
 		int width = Integer.parseInt(args[1]);
 
 		Rectangle myRectangle = new Rectangle(length, width);
